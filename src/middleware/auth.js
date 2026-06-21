b8491e7a9d33712345a8d74880a41cf58868b364
GvaroGG <giogvaramiaok@gmail.com>
2026-06-21 15:17:54 +0400

    user model, jwt auth middleware da request validacia

---
+++ src/middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const ApiError = require('../utils/ApiError');
+const asyncHandler = require('../utils/asyncHandler');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  const header = req.headers.authorization;
+  if (header && header.startsWith('Bearer ')) {
+    token = header.split(' ')[1];
+  }
+
+  if (!token) {
+    throw new ApiError(401, 'Not authorized, no token provided');
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await User.findById(decoded.id);
+  if (!user) {
+    throw new ApiError(401, 'Not authorized, user no longer exists');
+  }
+
+  req.user = user;
+  next();
+});
+
+const authorize = (...roles) => (req, res, next) => {
+  if (!req.user || !roles.includes(req.user.role)) {
+    return next(new ApiError(403, 'You do not have permission to perform this action'));
+  }
+  next();
+};
+
+module.exports = { protect, authorize };
